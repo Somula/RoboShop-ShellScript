14ce2d442fc3b22622ef105830f0d7ce1ca57d42
Somula <somulalingareddy2@gmail.com>
2024-07-06 19:07:23 +0530

    the location file is edited

--- user.sh
+++ user.sh
@@ -69,7 +69,7 @@ npm install  &>> $LOGFILE
 
 VALIDATE $? "Installing dependencies"
 
-cp /home/centos/roboshop-shell/user.service /etc/systemd/system/user.service
+cp /home/centos/RoboShop-ShellScript/user.service /etc/systemd/system/user.service
 
 VALIDATE $? "Copying user service file"
 
